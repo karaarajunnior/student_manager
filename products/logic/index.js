fa88457de9ea7c68a50ae30cf1a32235b1b644ba
kj <karaarajunior1@gmail.com>
2025-05-29 15:57:37 +0300

    day4

---
+++ products/logic/index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import products from "../products/data.js";
+import dotenv from "dotenv";
+dotenv.config();
+const port = process.env.port || 3000;
+
+const app = express();
+import cors from "cors";
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/products", (req, res) => {
+	const newproduct = products.map((product) => {
+		const { price, name, type } = product;
+		return { price, name, type };
+	});
+	res.json(newproduct);
+});
+
+app.get("/products/:id", (req, res) => {
+	const ID = req.params.id;
+	const single_product = products.find((product) => product.id === Number(ID));
+	res.json(single_product);
+});
+
+app.get("/product/query", (req, res) => {
+	const { search, limit } = req.query;
+	let sortedproducts = [...products];
+	if (search) {
+		sortedproducts = sortedproducts.filter((product) => {
+			return product.name.startsWith(search);
+		});
+
+		// res.send(sortedproducts);
+	}
+	if (limit) {
+		sortedproducts = sortedproducts.slice(0, Number(limit));
+	}
+	if (sortedproducts.length < 1) {
+		return res.json({ success: true, data: [] });
+	}
+	res.send(sortedproducts);
+});
+
+app.listen(port, () => {
+	console.log(`server running on port ${port}`);
+});
---
+++ products/products/data.js
@@ -0,0 +1,62 @@
+const products = [
+	{
+		id: 1,
+		price: 1000,
+		name: "bread",
+		type: {
+			brand: "ntake",
+			weight: "15g",
+		},
+	},
+
+	{
+		id: 2,
+		price: 2000,
+		name: "cake",
+		type: {
+			brand: "kikonyo",
+			weight: "0.5ml",
+		},
+	},
+
+	{
+		id: 3,
+		price: 2500,
+		name: "yorghut",
+		type: {
+			brand: "vanilla",
+			weight: "4g",
+		},
+	},
+
+	{
+		id: 4,
+		price: 3000,
+		name: "milk",
+		type: {
+			brand: "daily top",
+			weight: "12g",
+		},
+	},
+
+	{
+		id: 5,
+		price: 4000,
+		name: "icecream",
+		type: {
+			brand: "strawberry",
+			weight: "1g",
+		},
+	},
+
+	{
+		id: 6,
+		price: 5000,
+		name: "cornflakes",
+		type: {
+			brand: "yummy",
+			weight: "0.1g",
+		},
+	},
+];
+export default products;
